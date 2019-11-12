9c0a31fa358298d75d564a8d80b34a9a2687055f
kyle-n <kylebnazario@gmail.com>
2019-11-11 21:25:21 -0700

    Assorted folder for general components

---
+++ frontend/src/assorted/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+const Footer = () => (
+  <Typography variant="body2" style={{textAlign: 'center'}}>
+    Built by Kyle Nazario
+  </Typography>
+);
+
+export default Footer;
