7e44b4b0399ae6f9ab48432c86825cf68cf0d6a2
kyle-n <kylebnazario@gmail.com>
2019-11-11 21:07:03 -0700

    Main app displays theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
+
+import {themes} from './themes';
+
 function App() {
   return (
-    <div>
-    </div>
+    // Hardcoded to dark mode, but could refactor to accept a prop from redux or other
+    // global app state manager that would track the current theme
+    <ThemeProvider theme={themes['dark']}>
+      <CssBaseline>
+        <Container component="main">
+        </Container>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 
