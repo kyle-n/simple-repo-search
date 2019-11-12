fa584312556646f7a2385b80193984f59a1b24d0
kyle-n <kylebnazario@gmail.com>
2019-11-11 21:25:35 -0700

    Layout component for page

---
+++ frontend/src/repo-search-page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+
+// import SearchAreaContainer from './search-area-container';
+import {Footer} from './assorted';
+
+const RepoSearchPage = () => (
+  <Grid container>
+    <Grid item xs={12}>
+      {/* <SearchAreaContainer /> */}
+    </Grid>
+    <Grid item xs={12}>
+      <Footer />
+    </Grid>
+  </Grid>
+);
+
+export default RepoSearchPage;
