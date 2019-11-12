9303c79f660e4e3c8df06a56546fbb5903c79ff2
kyle-n <kylebnazario@gmail.com>
2019-11-11 21:32:39 -0700

    Site title component

---
+++ frontend/src/assorted/site-title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const SiteTitle = () => (
+  <header>
+    <Typography variant="h1">
+      Simple GitHub Repository Search
+    </Typography>
+  </header>
+);
+
+export default SiteTitle;
--- frontend/src/repo-search-page.js
+++ frontend/src/repo-search-page.js
@@ -4,10 +4,13 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
 // import SearchAreaContainer from './search-area-container';
-import {Footer} from './assorted';
+import {Footer, SiteTitle} from './assorted';
 
 const RepoSearchPage = () => (
   <Grid container>
+    <Grid item xs={12}>
+      <SiteTitle />
+    </Grid>
     <Grid item xs={12}>
       {/* <SearchAreaContainer /> */}
     </Grid>
