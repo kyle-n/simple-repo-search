989f35971a40b07d3be5374f9d3d989220b751b5
kyle-n <kylebnazario@gmail.com>
2019-11-11 18:38:21 -0700

    Hooks up api to search request

--- api/repos.js
+++ api/repos.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import {githubConnector} from '../external-connectors';
 
 const router = express.Router();
+
+const searchRepos = async (req, resp) => {
+  return await githubConnector.searchRepos(req._parsedUrl.query);
+};
+
+router.get('/search', searchRepos);
 
 export default router;
