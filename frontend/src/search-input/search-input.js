3333711859fef22db124251aee0b9aa053ad501a
kyle-n <kylebnazario@gmail.com>
2019-11-12 19:22:46 -0700

    Splits out search input into smaller components

--- frontend/src/search-input/search-input.js
+++ frontend/src/search-input/search-input.js
@@ -7,32 +7,45 @@ import SearchIcon from '@material-ui/icons/Search';
 import Grid from '@material-ui/core/Grid';
 
 const SearchInputBox = props => {
-  const inputId = 'repo-search-input';
   return (
     <Grid container>
       <Grid item xs={1}>
-        <SearchIcon />
+        <SearchIconBlock />
       </Grid>
       <Grid item xs={11}>
-        <FormControl>
-          <InputLabel htmlFor={inputId}>
-            Search
-          </InputLabel>
-          <Input id={inputId}
-                 type="text"
-                 name="Repository search input"
-                 autoFocus
-                 onChange={props.setQuery}
-                 endAdornment={(<Spinner loading={props.isLoading} />)}
-          />
-        </FormControl>
+        <SearchFormControl setQuery={props.setQuery}
+                           isLoading={props.isLoading} />
       </Grid>
     </Grid>
   );
 };
 
+const SearchFormControl = props => {
+  const inputId = 'repo-search-input';
+  return (
+    <FormControl>
+      <InputLabel htmlFor={inputId}>
+        Search
+      </InputLabel>
+      <Input id={inputId}
+              type="text"
+              name="Repository search input"
+              autoFocus
+              onChange={props.setQuery}
+              endAdornment={(<Spinner isLoading={props.isLoading} />)}
+      />
+    </FormControl>
+  );
+};
+
+const SearchIconBlock = () => (
+  <div style={{textAlign: 'center', margin: 'auto 0 0 0'}}>
+    <SearchIcon />
+  </div>
+);
+
 const Spinner = props => {
-  return props.loading ? (
+  return props.isLoading ? (
     <CircularProgress size={'1.25rem'}
                       style={{marginBottom: '0.5rem', marginRight: '0.5rem'}}
                       color="secondary"
