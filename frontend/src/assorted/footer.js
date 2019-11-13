0686629b4f25e447f49baddbc1e7c7fdef60de8a
kyle-n <kylebnazario@gmail.com>
2019-11-12 18:56:32 -0700

    Footer with layout and link to github

--- frontend/src/assorted/footer.js
+++ frontend/src/assorted/footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 
 const Footer = () => (
-  <Typography variant="body2" style={{textAlign: 'center'}}>
-    Built by Kyle Nazario
-  </Typography>
+  <footer>
+    <Box marginY="1rem">
+      <Box marginY="1rem">
+        <Divider variant="inset" />
+      </Box>
+      <Typography variant="body2" style={{textAlign: 'center'}}>
+        Built by <Link href="https://github.com/kyle-n" target="_blank">Kyle Nazario</Link>
+      </Typography>
+    </Box>
+  </footer>
 );
 
 export default Footer;
