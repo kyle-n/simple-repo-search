0b8b266fdc4f3c1f0bc62291c080c49328d6bc0c
kyle-n <kylebnazario@gmail.com>
2019-11-13 20:35:13 -0700

    Margins on site title

--- frontend/src/assorted/site-title.js
+++ frontend/src/assorted/site-title.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const SiteTitle = () => (
-  <header>
+  <Box component="header" marginY="1rem">
     <Typography variant="h1">
       Simple GitHub Repository Search
     </Typography>
-  </header>
+  </Box>
 );
 
 export default SiteTitle;
