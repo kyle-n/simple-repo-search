5f9b5c01543a8220ef01b8dc3fcfbec81bbfda68
kyle-n <kylebnazario@gmail.com>
2019-11-13 23:07:15 -0700

    Tests initial state of container

--- frontend/src/search-area-container.test.js
+++ frontend/src/search-area-container.test.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
 import ReactDOM from 'react-dom';
+import {act} from 'react-dom/test-utils';
+import {expect} from 'chai';
 import SearchAreaContainer from './search-area-container';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
 
 describe('search area container', () => {
 
@@ -15,6 +21,16 @@ describe('search area container', () => {
     ReactDOM.unmountComponentAtNode(container);
     container.remove();
     container = null;
+  });
+
+  it('should initialize with the correct default state', () => {
+    const wrapper = shallow(<SearchAreaContainer />);
+
+    expect(wrapper.state('searchResults')).to.be.an('array').of.length(0);
+    expect(wrapper.state('isLoading')).to.equal(false);
+    expect(wrapper.state('alert')).to.be.an('object');
+    expect(wrapper.state('alert').isError).to.equal(false);
+    expect(wrapper.state('alert').message).to.equal(null);
   });
 
 });
