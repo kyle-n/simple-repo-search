ff03b6f31899ef69b95147e4cc7a181d9fd00e17
kyle-n <kylebnazario@gmail.com>
2019-11-14 22:54:51 -0700

    Removes chai asserts

--- frontend/src/search-area-container.test.js
+++ frontend/src/search-area-container.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Enzyme, {shallow} from 'enzyme';
 import ReactDOM from 'react-dom';
-import chai from 'chai';
 import SearchAreaContainer from './search-area-container';
 import Adapter from 'enzyme-adapter-react-16';
 import * as ApiMethods from './api';
@@ -27,11 +26,9 @@ describe('search area container', () => {
   });
 
   it('should initialize with the correct default state', () => {
-    chai.expect(wrapper.state('searchResults')).to.be.an('array').of.length(0);
-    chai.expect(wrapper.state('isLoading')).to.equal(false);
-    chai.expect(wrapper.state('alert')).to.be.an('object');
-    chai.expect(wrapper.state('alert').isError).to.equal(false);
-    chai.expect(wrapper.state('alert').message).to.equal(null);
+    expect(wrapper.state('searchResults').length).toBe(0)
+    expect(wrapper.state('isLoading')).toBe(false);
+    expect(wrapper.state('alert')).toEqual({isError: false, message: null});
   });
 
   it('should not search if given an empty string query', () => {
