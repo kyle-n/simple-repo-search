44b9f359b21e4f9ae36de808d5f489847b617920
kyle-n <kylebnazario@gmail.com>
2019-11-16 08:59:59 -0700

    Light blue primary theme color

--- frontend/src/themes/themes.model.js
+++ frontend/src/themes/themes.model.js
@@ -1,8 +1,10 @@
 import {createMuiTheme} from '@material-ui/core';
+import lightBlue from '@material-ui/core/colors/lightBlue'
 
 const dark = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: lightBlue
   },
   typography: {
     h1: {
