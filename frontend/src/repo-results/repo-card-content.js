f8a5d7e13060d93f5b742d8186dce2ee1ee9a641
kyle-n <kylebnazario@gmail.com>
2019-11-15 23:59:52 -0700

    Moves devicon css into only its component

--- frontend/src/repo-results/repo-card-content.js
+++ frontend/src/repo-results/repo-card-content.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import 'devicon/devicon.min.css'
 
 const RepoCardContent = props => (
   <CardContent>
