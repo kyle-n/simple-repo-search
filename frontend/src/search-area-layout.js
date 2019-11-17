101e4d75d7e954e0a6923f4b1eca50455b6a9e0f
kyle-n <kylebnazario@gmail.com>
2019-11-17 12:43:48 -0700

    Spacing

--- frontend/src/search-area-layout.js
+++ frontend/src/search-area-layout.js
@@ -7,23 +7,23 @@ import {Alert} from './assorted';
 
 const SearchAreaLayout = props => (
   <Box minHeight="10rem">
-  <Grid container justify="center">
-    <Grid item xs={12} md={6}>
-      <SearchInput setQuery={props.setQuery}
-                   isLoading={props.isLoading}
-                   setFilter={props.setFilter}
+    <Grid container justify="center">
+      <Grid item xs={12} md={6}>
+        <SearchInput setQuery={props.setQuery}
+                     isLoading={props.isLoading}
+                     setFilter={props.setFilter}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Box marginY="1rem">
+          <ResultList results={props.searchResults} />
+        </Box>
+      </Grid>
+      {/* In a larger application, this component would be root-level and accept global state values */}
+      <Alert message={props.alert.message}
+             isError={props.alert.isError}
       />
     </Grid>
-    <Grid item xs={12}>
-      <Box marginY="1rem">
-        <ResultList results={props.searchResults} />
-      </Box>
-    </Grid>
-    {/* In a larger application, this component would be root-level and accept global state values */}
-    <Alert message={props.alert.message}
-           isError={props.alert.isError}
-    />
-  </Grid>
   </Box>
 );
 
