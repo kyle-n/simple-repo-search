dd7cc6db5909d7b64f05fe5dbbf321bfd6df89b3
kyle-n <kylebnazario@gmail.com>
2019-11-23 18:01:33 -0700

    Adjusts language

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ import apiRouter from './api';
 
 // server config
 const reactFrontendBuildDirectory = '/react-frontend/build';
+const elmFrontendBuildDirectory = '/elm-frontend';
 dotenv.config(__dirname + '/.env');
 const server = express();
 server.get('/materialize-css/:filename', (req, resp) => {
   return resp.sendFile(path.join(__dirname + '/node_modules/materialize-css/dist/css/' + req.params.filename));
+});
+server.get('/elm/static/:filename', (req, resp) => {
+  return resp.sendFile(path.join(__dirname + '/elm-frontend/' + req.params.filename));
 });
 server.use(bodyParser.json({limit: '1mb'}));
 server.engine('html', ejs.renderFile);
@@ -40,12 +44,20 @@ server.get('/', (req, resp) => {
 server.use(express.static(path.join(__dirname + reactFrontendBuildDirectory)));
 
 // serve frontends
+
+// react
 const serveReact = (req, resp) => {
   return resp.sendFile(path.join(__dirname + reactFrontendBuildDirectory + '/index.html'));
-  // resp.render(path.join(__dirname + reactFrontendBuildDirectory + '/index'), { req, resp });
 };
 server.get('/react', serveReact);
 server.get('/react/*', serveReact);
+
+// elm
+const serveElm = (req, resp) => {
+  return resp.sendFile(path.join(__dirname + elmFrontendBuildDirectory + '/index.html'));
+};
+server.get('/elm', serveElm);
+server.get('/elm/*', serveElm);
 
 // listen
 const port = process.env.PORT || 8000;
