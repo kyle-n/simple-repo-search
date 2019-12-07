36282f58b0dfe7963869351a65e7fbd91f31c73e
kyle-n <kylenazario@scadmins-MacBook-Pro.local>
2019-12-07 11:57:29 -0700

    Maps cards to two-card rows

--- elm-frontend/src/Results/Layout.elm
+++ elm-frontend/src/Results/Layout.elm
@@ -1,11 +1,17 @@
 module Results.Layout exposing (viewLayout)
 
+
 import Html exposing (Html, div)
 import Html.Attributes exposing (class)
 import Results.Card
 import Types exposing (Repo)
+import List.Split exposing (chunksOfLeft)
 
 
 viewLayout : List Repo -> Html msg
 viewLayout repos =
-    div [ class "row" ] (List.map Results.Card.viewCard repos)
+    let
+        cardPairs = chunksOfLeft 2 (List.map Results.Card.viewCard repos)
+        rows = List.map (\cardPair -> div [ class "row" ] cardPair ) cardPairs
+    in
+    div [ class "row" ] rows
